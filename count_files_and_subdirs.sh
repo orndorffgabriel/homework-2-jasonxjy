0b775085ca9447c3fe6d3585ba15bc1cae09545d
jasonxjy <jasonxjy@ksu.edu>
2019-09-20 11:52:34 -0500

    adding HW2 part 2 new version

--- count_files_and_subdirs.sh
+++ count_files_and_subdirs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-x=' find -maydepth 1 -type d -print| wc -l'
-y=' find -maxdepth 1 -type f -print| wc -l'
+
+
+x=$(find -maxdepth 1 -type d -print| wc -l)
+y=$(find -maxdepth 1 -type f -print| wc -l)
 
 echo you have $y files and $x subdirectories in current directory including hidden items and .. directory. 
